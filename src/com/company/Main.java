9316b15b200ec327e62a0b7ec7e0978b5da06ef3
Your GITHUB Name <GITHUB you@example.com>
2018-11-11 20:47:57 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Athlete athlete=new Athlete();
+        athlete.setDataTheAthlete(5555,"Mac Li","USA","biathlon", (short) 3);
+        athlete.getDataTheAthlete();
+
+    }
+}
